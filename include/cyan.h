b88d7acc71d84899796920c7ae78b9f286625277
davidmkwon <surval2001@gmail.com>
2023-04-15 20:36:25 -0400

    add workers
    but not really working right because mutex priority is wonky and only
    one worker is getting access while others are all starved

--- include/cyan.h
+++ include/cyan.h
@@ -1,10 +1,14 @@
 #include <atomic>
+#include <deque>
 #include <functional>
 #include <iostream>
+#include <shared_mutex>
 #include <stdexcept>
 #include <string>
 #include <sys/event.h>
+#include <thread>
 #include <unordered_map>
+#include <vector>
 
 namespace cyan {
 
@@ -14,8 +18,83 @@ enum Interest { Readable, Writable };
 // function type for the handler functions invoked on an event
 using Handler = std::function<void()>;
 
-// responsible for tracking event changes and dispatching to the respective handlers
+// responsible for tracking event changes and dispatching to the respective
+// handlers
 class Executor {
+
+private:
+  // Worker manages a thread that pulls from an Executor's work queue and runs it
+  class Worker {
+  public:
+    // an empty worker, with the thread doing nothing
+    Worker(std::size_t id) : _keep_running(true), _id(id) { }
+
+    // move constructs a worker, taking ownership of the underlying thread
+    Worker(Worker &&other) {
+      _keep_running = other._keep_running.load();
+      _t = std::move(other._t);
+      _id = other._id;
+    }
+
+    // safety of holding a reference to exec--Executor, upon destruction, calls the
+    // destructor of all its Workers, which will stop the running thread (the referencer
+    // of exec) and thus it will be safe to destroy exec
+    void start(Executor &exec) {
+      _t = std::thread([&](std::size_t _id) {
+          Executor::Token tok;
+          Handler handler;
+
+          // check kill signal
+          while (_keep_running.load()) {
+            // check if there is work to do
+            exec._wqm.lock();
+            if (!exec._wq.empty()) {
+              // pop off the next token and free the lock
+              tok = exec._wq.front();
+              std::cout << "Worker " << _id << " processing tok " << tok << std::endl;
+              exec._wq.pop_front();
+              exec._wqm.unlock();
+
+              // get read handle to handler map and call handler
+              exec._hsm.lock_shared();
+              handler = exec._handlers[tok];
+              exec._hsm.unlock_shared();
+
+              // call handler
+              handler();
+            }
+            // don't forget to free the lock :D
+            else {
+              exec._wqm.unlock();
+            }
+          }
+      }, _id);
+    }
+
+    // stops the worker
+    void stop() {
+      _keep_running.store(false);
+      if (_t.joinable()) {
+        _t.join();
+      }
+    }
+
+    ~Worker() {
+      stop();
+    }
+
+  public:
+    // keeps the thread loop running
+    std::atomic<bool> _keep_running;
+
+    // handle to thread
+    std::thread _t;
+
+    // worker id
+    std::size_t _id;
+  };
+
+
 public:
   Executor() {
     // initialize a kqueue
@@ -24,11 +103,12 @@ public:
     }
   }
 
-  // registers a handler with the executor for the given file descriptor and its interest
+  // registers a handler with the executor for the given file descriptor and its
+  // interest
   //
-  // currently, the handler will simply be invoked as soon as there is a read/write event
-  // for the fd. this means that it is the handler's responsibility to appropriately
-  // read/write from the fd.
+  // currently, the handler will simply be invoked as soon as there is a
+  // read/write event for the fd. this means that it is the handler's
+  // responsibility to appropriately read/write from the fd.
   void register_handler(int fd, Interest interest, Handler handler) {
     // fill in kevent struct
     struct kevent ke;
@@ -37,22 +117,31 @@ public:
     auto tok = next_token();
     EV_SET(&ke, fd, filter, flags, 0, 0, (void *)tok);
 
-    // map handler to generated token
+    // map handler to generated token, get write lock
+    _hsm.lock();
     _handlers[tok] = handler;
+    _hsm.unlock();
 
     // register kevent with kqueue
-    int ret;
-    if ((ret = kevent(_kq, &ke, 1, nullptr, 0, nullptr)) == -1) {
+    if (kevent(_kq, &ke, 1, nullptr, 0, nullptr) == -1) {
       throw std::runtime_error("could not add kevent to kqueue");
     }
   }
 
   // runs the executor, polling for event changes and calling the respective handlers
   void run() {
-    int ret;
+    // start one (arbitrary) worker
+    //
+    // TODO: make this number of workers dynamically scale
+    for (std::size_t i = 0; i < 2; i++) {
+      _workers.push_back(Worker(i));
+      std::cout << _workers[i]._id << std::endl;
+      _workers[i].start(*this);
+    }
 
     while (true) {
       // wait for an event to trigger, blocking indefinitely
+      int ret;
       if ((ret = kevent(_kq, nullptr, 0, _kevents, KEVENTS_SIZE, nullptr)) == -1) {
         throw std::runtime_error(std::string("kevent error: ") + strerror(errno));
       }
@@ -61,7 +150,9 @@ public:
       for (int i = 0; i < ret; i++) {
         // parse out token
         auto tok = (Token)_kevents[i].udata;
-        _handlers[tok]();
+
+        // send to workers
+        dispatch(tok);
       }
     }
   }
@@ -72,30 +163,46 @@ private:
 
   // returns the next token for use
   //
-  // simply atomically increments a counter to use as the token
+  // atomically increments a counter to use as the token
   Token next_token() {
     static std::atomic<std::size_t> curr = 1;
 
     return curr.fetch_add(1, std::memory_order_relaxed);
   }
 
+  // idk yet but this needs to call the handler for this token asynchronously,
+  // probs cud add it to a shared worker queue? idk if you should add the token
+  // or the handler function itself to the queue
+  void dispatch(Token tok) {
+    std::lock_guard<std::mutex> guard(_wqm);
+    _wq.push_back(tok);
+  }
+
 private:
   // handle to kqueue
   int _kq;
 
-  // array of kevent structs to retrieve results of kevent syscall
-  //
-  // TODO: make this size dynamic, based on the number of events registered?
+  // array of kevent structs to retrieve results of kevent syscall. 24 is arbitrary
   static constexpr std::size_t KEVENTS_SIZE = 24;
   struct kevent _kevents[KEVENTS_SIZE];
 
-  // tracks the handler for each event registered in the executor
+  // task queue for workers
+  std::deque<Token> _wq;
+  std::mutex _wqm;
+
+  // holds worker handles
+  std::vector<Worker> _workers;
+
+  // tracks the handler for each event registered in the executor. read/write lock
+  // protection, should be read-heavy
   std::unordered_map<Token, Handler> _handlers;
+  std::shared_mutex _hsm;
 };
+
 
 void on_event(int fd, Interest interest, Handler handler) {
   // reference to global executor
   static Executor _exec;
 }
 
-};
+}; // namespace cyan
--- test/cyan_tests.cc
+++ test/cyan_tests.cc
@@ -12,34 +12,47 @@ void die(const char *msg) {
 int main() {
   cyan::Executor exec;
 
-  // create a pipe to read/write
-  int fds[2];
-  if (pipe(fds) == -1) {
+  // create two pipes to read/write
+  int fds1[2];
+  int fds2[2];
+  if (pipe(fds1) == -1) {
+    die("pipe didn't work");
+  }
+  if (pipe(fds2) == -1) {
     die("pipe didn't work");
   }
 
-  // create handler to print out message written to pipe
-  int read_fd = fds[0];
-  cyan::Handler h = [=]() {
+  // create handlers to print out message written to pipes
+  int read_fd1 = fds1[0];
+  cyan::Handler h1 = [=]() {
     char buf[6] = { '\0' };
-    read(read_fd, buf, 5);
-    std::cout << "read out: " << buf << std::endl;
+    read(read_fd1, buf, 5);
+    std::cout << "read from pipe1: " << buf << std::endl;
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(1000ms);
   };
-  exec.register_handler(read_fd, cyan::Interest::Readable, h);
+  exec.register_handler(read_fd1, cyan::Interest::Readable, h1);
 
-  // spawn a thread that waits and writes to pipe
-  int write_fd = fds[1];
+  int read_fd2 = fds2[0];
+  cyan::Handler h2 = [=]() {
+    char buf[6] = { '\0' };
+    read(read_fd2, buf, 5);
+    std::cout << "read from pipe2: " << buf << std::endl;
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(1000ms);
+  };
+  exec.register_handler(read_fd2, cyan::Interest::Readable, h2);
+
+  // spawn a thread that waits and writes to the two pipes
+  int write_fd1 = fds1[1];
+  int write_fd2 = fds2[1];
   std::thread t([=] {
       using namespace std::chrono_literals;
-      std::this_thread::sleep_for(1000ms);
-
-      write(write_fd, "hello", 5);
-
-      std::this_thread::sleep_for(1000ms);
-
-      write(write_fd, "world", 5);
-
-      std::this_thread::sleep_for(1000ms);
+      while (true) {
+        std::this_thread::sleep_for(500ms);
+        write(write_fd1, "hello", 5);
+        write(write_fd2, "world", 5);
+      }
   });
 
   exec.run();
